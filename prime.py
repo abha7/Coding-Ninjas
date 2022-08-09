802829eec2b5416a4ed81fc92456e350ec8cf0ac
Abha <abhalad29@gmail.com>
2022-08-09 19:51:53 +0530

    first commit

---
+++ prime.py
@@ -0,0 +1,30 @@
+# taking input from the user
+number = int(input("Enter any number: "))
+isPrime= True #Boolean to store if number is prime or not
+if number > 1: # prime number is always greater than 1
+   i=2
+   while i< number:
+      if (number % i) == 0: # Checking for positive divisors
+        isPrime= False
+        break
+      i=i+1
+if(number<=1): # If number is less than or equal to 1
+   print("Is Not Prime")
+elif(isPrime): # If Boolean is true
+   print("Is Prime")
+else:
+   print("Is Not Prime")
+
+
+
+# num = int(input("Enter the number: "))
+# prime=True
+
+# for i in range(2, num):
+#     if(num%i == 0):
+#         prime = False
+#         break
+# if prime:
+#     print("This number is Prime")
+# else:
+#     print("This number is not Prime")
---
+++ sum.py
@@ -0,0 +1,29 @@
+n=4
+sum=0
+for i in range(n+1):
+    sum=sum+i
+print(sum)
+
+# n=4
+# sum=0
+# i=1
+# while(i<=n):
+#     sum=sum+i
+#     i=i+1
+# print(sum)
+
+# sum of n natural nos
+
+# n = int(input("enter any no : "))
+# sum = 0
+# for i in range(n+1):
+#     sum =  sum+i
+# print(f"sum of natural nos till {n} is {sum}")
+
+
+# num=int(input())
+# sum=0
+# while num > 0:
+#     sum += num
+#     num -= 1
+# print(sum)
